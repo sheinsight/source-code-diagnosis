4264f20ba9b42fbc578b6264313a7ad644e3f9ac
ityuany <519495771@qq.com>
2024-08-05 20:35:27 +0800

    add boolean_literals

---
+++ src/syntax/grammar/boolean_literals.rs
@@ -0,0 +1,86 @@
+use std::marker::PhantomData;
+
+use oxc_ast::{visit::walk, AstKind, Visit};
+use oxc_span::Span;
+use serde_json::from_str;
+
+use crate::syntax::{
+  compat::{Compat, CompatBox},
+  operators::common_trait::CommonTrait,
+};
+
+pub struct BooleanLiteralsVisitor<'a> {
+  pub cache: Vec<CompatBox>,
+  parent_stack: Vec<AstKind<'a>>,
+  source_code: &'a str,
+  _phantom: PhantomData<&'a ()>,
+  compat: Compat,
+}
+
+impl CommonTrait for BooleanLiteralsVisitor<'_> {
+  fn get_cache(&self) -> Vec<CompatBox> {
+    self.cache.clone()
+  }
+}
+
+impl<'a> BooleanLiteralsVisitor<'a> {
+  pub fn new(source_code: &'a str) -> Self {
+    let compat: Compat =
+      from_str(include_str!("./boolean_literals.json")).unwrap();
+    Self {
+      cache: Vec::new(),
+      parent_stack: Vec::new(),
+      source_code,
+      _phantom: PhantomData {},
+      compat: compat,
+    }
+  }
+
+  fn get_source_code(&self, span: Span) -> &str {
+    &self.source_code[span.start as usize..span.end as usize]
+  }
+}
+
+impl<'a> Visit<'a> for BooleanLiteralsVisitor<'a> {
+  fn enter_node(&mut self, kind: oxc_ast::AstKind<'a>) {
+    self.parent_stack.push(kind);
+  }
+
+  fn leave_node(&mut self, _kind: oxc_ast::AstKind<'a>) {
+    self.parent_stack.pop();
+  }
+
+  fn visit_boolean_literal(&mut self, it: &oxc_ast::ast::BooleanLiteral) {
+    self.cache.push(CompatBox {
+      start: it.span.start,
+      end: it.span.end,
+      code_seg: self.get_source_code(it.span).to_string(),
+      compat: self.compat.clone(),
+    });
+    walk::walk_boolean_literal(self, it);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+
+  use crate::syntax::operators::t::t_any;
+  use oxc_allocator::Allocator;
+
+  use super::*;
+
+  #[test]
+  fn should_test() {
+    let source_code = r##"
+true
+false    
+"##;
+    let allocator = Allocator::default();
+    t_any(
+      "boolean_literals",
+      source_code,
+      &allocator,
+      BooleanLiteralsVisitor::new,
+    );
+  }
+}
--- src/syntax/grammar/mod.rs
+++ src/syntax/grammar/mod.rs
@@ -1,2 +1,3 @@
 pub mod array_literals;
 pub mod binary_numeric_literals;
+pub mod boolean_literals;
