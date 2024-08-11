c106630a6e430a2314828fa772c26b7836005e36
ityuany <519495771@qq.com>
2024-08-11 21:54:44 +0800

    modify tmp

--- src/syntax/tmp/tmp.rs
+++ src/syntax/tmp/tmp.rs
@@ -6,26 +6,39 @@ use crate::syntax::{
   compat::{Compat, CompatBox},
 };
 
-pub struct TmpVisitor {
+pub struct TmpVisitor<'a> {
   usage: Vec<CompatBox>,
+  parent_stack: Vec<AstKind<'a>>,
   compat: Compat,
 }
 
-impl Default for TmpVisitor {
+impl<'a> Default for TmpVisitor<'a> {
   fn default() -> Self {
     let usage: Vec<CompatBox> = Vec::new();
     let compat: Compat = from_str(include_str!("./tmp.json")).unwrap();
-    Self { usage, compat }
+    Self {
+      usage,
+      compat,
+      parent_stack: Vec::new(),
+    }
   }
 }
 
-impl CommonTrait for TmpVisitor {
+impl<'a> CommonTrait for TmpVisitor<'a> {
   fn get_usage(&self) -> Vec<CompatBox> {
     self.usage.clone()
   }
 }
 
-impl<'a> Visit<'a> for TmpVisitor {}
+impl<'a> Visit<'a> for TmpVisitor<'a> {
+  fn enter_node(&mut self, kind: oxc_ast::AstKind<'a>) {
+    self.parent_stack.push(kind);
+  }
+
+  fn leave_node(&mut self, _kind: oxc_ast::AstKind<'a>) {
+    self.parent_stack.pop();
+  }
+}
 
 #[cfg(test)]
 mod tests {
@@ -41,7 +54,7 @@ mod tests {
   #[test]
   fn should_ok_when_async_generator_function_declaration() {
     let mut tester = SemanticTester::from_visitor(TmpVisitor::default());
-    let usage = tester.analyze("async function* foo() {}");
+    let usage = tester.analyze("");
 
     let count = get_async_function_count(&usage);
 
