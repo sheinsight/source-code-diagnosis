051811ebd596da66afb1d8b294641e617e34e2a9
ityuany <519495771@qq.com>
2024-08-12 10:31:17 +0800

    add boolean_literals

---
+++ src/syntax/grammar/boolean_literals.rs
@@ -0,0 +1,82 @@
+use oxc_ast::{visit::walk, AstKind, Visit};
+use serde_json5::from_str;
+
+use crate::syntax::{
+  common::CommonTrait,
+  compat::{Compat, CompatBox},
+};
+
+pub struct BooleanLiteralsVisitor<'a> {
+  usage: Vec<CompatBox>,
+  parent_stack: Vec<AstKind<'a>>,
+  compat: Compat,
+}
+
+impl<'a> Default for BooleanLiteralsVisitor<'a> {
+  fn default() -> Self {
+    let usage: Vec<CompatBox> = Vec::new();
+    let compat: Compat =
+      from_str(include_str!("./boolean_literals.json")).unwrap();
+    Self {
+      usage,
+      compat,
+      parent_stack: Vec::new(),
+    }
+  }
+}
+
+impl<'a> CommonTrait for BooleanLiteralsVisitor<'a> {
+  fn get_usage(&self) -> Vec<CompatBox> {
+    self.usage.clone()
+  }
+}
+
+impl<'a> Visit<'a> for BooleanLiteralsVisitor<'a> {
+  fn enter_node(&mut self, kind: oxc_ast::AstKind<'a>) {
+    self.parent_stack.push(kind);
+  }
+
+  fn leave_node(&mut self, _kind: oxc_ast::AstKind<'a>) {
+    self.parent_stack.pop();
+  }
+
+  fn visit_boolean_literal(&mut self, it: &oxc_ast::ast::BooleanLiteral) {
+    self
+      .usage
+      .push(CompatBox::new(it.span.clone(), self.compat.clone()));
+    walk::walk_boolean_literal(self, it);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+
+  use crate::syntax::semantic_tester::SemanticTester;
+
+  use super::*;
+
+  fn get_async_function_count(usage: &Vec<CompatBox>) -> usize {
+    usage
+      .iter()
+      .filter(|item| item.name == "boolean_literals")
+      .count()
+  }
+
+  #[test]
+  fn should_ok_when_async_generator_function_declaration() {
+    let mut tester =
+      SemanticTester::from_visitor(BooleanLiteralsVisitor::default());
+    let usage = tester.analyze(
+      "
+true;
+false;
+",
+    );
+
+    let count = get_async_function_count(&usage);
+
+    assert_eq!(usage.len(), 2);
+
+    assert_eq!(count, 2);
+  }
+}
