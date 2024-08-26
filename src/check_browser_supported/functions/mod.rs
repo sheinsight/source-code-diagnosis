eff7c30154b5695367110d691fa6ae29ba819821
ityuany <519495771@qq.com>
2024-08-26 20:48:34 +0800

    remove compat when chrome eq 1

--- src/check_browser_supported/functions/mod.rs
+++ src/check_browser_supported/functions/mod.rs
@@ -22,18 +22,18 @@ pub mod set;
 pub mod set_computed_property_names;
 
 pub fn setup_functions(v: &mut SyntaxVisitor) {
-  arguments_callee::setup(v);
+  // arguments_callee::setup(v);
   arguments_iterator::setup(v);
-  arguments_length::setup(v);
+  // arguments_length::setup(v);
   arrow_functions_trailing_comma::setup(v);
   arrow_functions::setup(v);
   block_level_functions::setup(v);
   default_parameters_destructured_parameter_with_default_value_assignment::setup(v);
   default_parameters_parameters_without_defaults_after_default_parameters::setup(v);
   default_parameters::setup(v);
-  functions::setup(v);
+  // functions::setup(v);
   get_computed_property_names::setup(v);
-  get::setup(v);
+  // get::setup(v);
   method_definitions_async_generator_methods::setup(v);
   method_definitions_async_methods::setup(v);
   method_definitions_generator_methods_not_constructable::setup(v);
@@ -41,5 +41,5 @@ pub fn setup_functions(v: &mut SyntaxVisitor) {
   rest_parameters_destructuring::setup(v);
   rest_parameters::setup(v);
   set_computed_property_names::setup(v);
-  set::setup(v);
+  // set::setup(v);
 }
--- src/check_browser_supported/grammar/mod.rs
+++ src/check_browser_supported/grammar/mod.rs
@@ -23,25 +23,25 @@ pub mod unicode_escape_sequences;
 pub mod unicode_point_escapes;
 
 pub fn setup_grammar(v: &mut SyntaxVisitor) {
-  array_literals::setup(v);
+  // array_literals::setup(v);
   binary_numeric_literals::setup(v);
-  boolean_literals::setup(v);
-  decimal_numeric_literals::setup(v);
+  // boolean_literals::setup(v);
+  // decimal_numeric_literals::setup(v);
   hashbang_comments::setup(v);
   // hexadecimal_escape_sequences::setup_hexadecimal_escape_sequences(v);
-  hexadecimal_numeric_literals::setup(v);
-  null_literal::setup(v);
+  // hexadecimal_numeric_literals::setup(v);
+  // null_literal::setup(v);
   numeric_separators::setup(v);
   octal_numeric_literals::setup(v);
-  regular_expression_literals::setup(v);
+  // regular_expression_literals::setup(v);
   shorthand_object_literals::setup(v);
-  string_literals::setup(v);
+  // string_literals::setup(v);
   template_literals_template_literal_revision::setup(v);
   template_literals::setup(v);
   trailing_commas_trailing_commas_in_dynamic_import::setup(v);
   trailing_commas_trailing_commas_in_functions::setup(v);
-  trailing_commas_trailing_commas_in_object_literals::setup(v);
-  trailing_commas::setup(v);
+  // trailing_commas_trailing_commas_in_object_literals::setup(v);
+  // trailing_commas::setup(v);
   // unicode_escape_sequences::setup_unicode_escape_sequences(v);
   // unicode_point_escapes::setup_unicode_point_escapes(v);
 }
