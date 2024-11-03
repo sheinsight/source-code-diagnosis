acfd71d753fcc69ff6d9115c76f8def0757f3d90
ityuany <519495771@qq.com>
2024-11-04 00:52:57 +0800

    rename test

--- __test__/check_phantom_dependencies/index.spec.ts
+++ __test__/check_phantom_dependencies/index.spec.ts
@@ -6,13 +6,13 @@ import { dirname } from "node:path";
 
 const __filename = fileURLToPath(import.meta.url);
 
-test("checkPhantomDependencies", async () => {
+test("No phantom dependencies", async () => {
 	const cwd = path.resolve(dirname(__filename), "features", "demo1");
 	const res = await checkPhantomDependencies(["react"], { cwd });
 	expect(res.graph.length).toBe(0);
 });
 
-test("checkPhantomDependencies", async () => {
+test("The existence of phantom dependencies", async () => {
 	const cwd = path.resolve(dirname(__filename), "features", "demo2");
 	const res = await checkPhantomDependencies(["react"], { cwd });
 
