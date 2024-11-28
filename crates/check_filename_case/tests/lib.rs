63d41af0b89405b3fdb09ff63524d2b866b198f1
ityuany <519495771@qq.com>
2024-11-28 16:54:49 +0800

    add testing

---
+++ crates/check_filename_case/tests/lib.rs
@@ -0,0 +1,18 @@
+use check_filename_case::{check_filename_case, Args, JsArgs};
+
+#[test]
+fn test_check_filename_case() -> anyhow::Result<()> {
+  let cwd = std::env::current_dir()?;
+
+  let args = Args::from(JsArgs {
+    cwd: cwd.to_string_lossy().to_string(),
+    pattern: None,
+    ignore: None,
+  });
+
+  let res = check_filename_case(args)?;
+
+  assert_eq!(res.len(), 1);
+
+  Ok(())
+}
